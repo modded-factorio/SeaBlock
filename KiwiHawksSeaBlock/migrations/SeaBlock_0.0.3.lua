c8894a1652a9fed3d68bd59e7fca11ee6988c9eb
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-12-10 22:52:17 +0000

    Reverting changes
    This will no longer be a mod fork so reverting naming changes

---
+++ KiwiHawksSeaBlock/migrations/SeaBlock_0.0.3.lua
@@ -0,0 +1,6 @@
+--for _, force in pairs(game.forces) do
+--  force.reset_technologies()
+--  if force.technologies['electronics'].researched then
+--    force.recipes['solder-alginic'].enabled = true
+--  end
+--end
