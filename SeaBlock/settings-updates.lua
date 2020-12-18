280e4d6826c71c9a89613f1c8b1655936a1a9c0d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-12-19 10:34:45 +1300

    Resolves #18

--- SeaBlock/settings-updates.lua
+++ SeaBlock/settings-updates.lua
@@ -95,3 +95,7 @@ end
 if data.raw['bool-setting']['bobmods-assembly-burner'] then
    data.raw['bool-setting']['bobmods-assembly-burner'].default_value = false
 end
+
+if data.raw['bool-setting']['angels-enable-converter'] then
+   data.raw['bool-setting']['angels-enable-converter'].default_value = false
+end
