d284068f612f96afbc75b0be7cd99460efb765f7
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-12-19 05:08:22 +1300

    Fixes #22
    Clear all unwanted presets rather than a named list.

--- SeaBlock/data-final-fixes/mapgen.lua
+++ SeaBlock/data-final-fixes/mapgen.lua
@@ -35,9 +35,10 @@ for k,v in pairs(controls) do
   end
 end
 
-data.raw['map-gen-presets']['default']['death-world'] = nil
-data.raw['map-gen-presets']['default']['death-world-marathon'] = nil
-data.raw['map-gen-presets']['default']['rail-world'] = nil
-data.raw['map-gen-presets']['default']['rich-resources'] = nil
-data.raw['map-gen-presets']['default']['ribbon-world'] = nil
-data.raw['map-gen-presets']['default']['island'] = nil
+local presets = data.raw['map-gen-presets']['default']
+for k,v in pairs(presets) do
+  -- Check for order as this is a manditory property for a MapGenPreset (so we skip type and name)
+  if k ~= 'default' and k ~= 'marathon' and v.order then
+    data.raw['map-gen-presets']['default'][k] = nil
+  end
+end
