cc2aa228282b7de7205e5d4d716e102ada84acc2
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-12-22 23:05:18 +1300

    Set setting Science Pack colors update default value to false #36
    Bob's Tech setting Science Pack colors update defalut set to false

--- SeaBlock/settings-updates.lua
+++ SeaBlock/settings-updates.lua
@@ -5,3 +5,7 @@ end
 if data.raw['bool-setting']['angels-enable-converter'] then
    data.raw['bool-setting']['angels-enable-converter'].default_value = false
 end
+
+if data.raw['bool-setting']['bobmods-tech-colorupdate'] then
+  data.raw['bool-setting']['bobmods-tech-colorupdate'].default_value = false
+end
