6f818ab52199bda1276322951dade6e31b3bb38b
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-02-10 00:15:48 +1300

    Cobalt Processing cannot be researched #98

--- SeaBlock/data-final-fixes/tech-tree.lua
+++ SeaBlock/data-final-fixes/tech-tree.lua
@@ -1,6 +1,8 @@
 local lib = require "lib"
 
 -- Remove empty bob's techs
+bobmods.lib.tech.remove_prerequisite('cobalt-processing', 'chemical-processing-1')
+bobmods.lib.tech.remove_prerequisite('cobalt-processing', 'electrolysis-1')
 bobmods.lib.tech.remove_prerequisite('lithium-processing', 'electrolysis-1')
 bobmods.lib.tech.replace_prerequisite('lithium-processing', 'chemical-processing-1', 'chlorine-processing-3')
 bobmods.lib.tech.add_new_science_pack('lithium-processing', 'chemical-science-pack', 1)
