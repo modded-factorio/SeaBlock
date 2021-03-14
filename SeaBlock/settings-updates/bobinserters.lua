79c7afe902aba95376f1219a0a525deeb4823500
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-03-14 19:30:00 +1300

    Revert inserters override #116
    Revert Bob's Inserters default settings override

--- SeaBlock/settings-updates/bobinserters.lua
+++
@@ -1,5 +0,0 @@
--- Bob's Adjustable Inserters mod
-if mods['bobinserters'] then
-  seablock.set_setting_default_value('bool-setting', 'bobmods-inserters-long2', false)
-  seablock.set_setting_default_value('bool-setting', 'bobmods-inserters-more2', false)
-end
