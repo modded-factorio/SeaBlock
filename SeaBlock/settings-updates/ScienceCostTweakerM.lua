688e7286395ee2934e0877f7da4b8e98caf67b0c
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-05-21 18:15:04 +1200

    Settings updates

--- SeaBlock/settings-updates/ScienceCostTweakerM.lua
+++ SeaBlock/settings-updates/ScienceCostTweakerM.lua
@@ -3,4 +3,5 @@ if mods['ScienceCostTweakerM'] then
   seablock.overwrite_setting('string-setting', 'sct-lab-modules', 'none')
   seablock.overwrite_setting('bool-setting', 'sct-connect-science', false)
   seablock.overwrite_setting('string-setting', 'sct-bio', 'tier1')
+  seablock.overwrite_setting('string-setting', 'sct-tier1-lab', 'sct-lab-1')
 end
