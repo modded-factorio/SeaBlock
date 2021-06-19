2f43d6638eb821f5f7f7dad2b1edfcbe30c9bda9
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-06-20 10:58:09 +1200

    Algae farm recipes and tech unlocks #140

--- SeaBlock/data-updates/algae.lua
+++ SeaBlock/data-updates/algae.lua
@@ -5,10 +5,11 @@ data.raw['assembling-machine']['algae-farm'].crafting_speed = 0.75
 bobmods.lib.tech.add_prerequisite('bio-processing-brown', 'bio-processing-green')
 bobmods.lib.tech.remove_recipe_unlock('bio-processing-brown', 'algae-farm')
 bobmods.lib.tech.remove_recipe_unlock('bio-processing-brown', 'algae-green-simple')
-bobmods.lib.tech.remove_recipe_unlock('bio-processing-green', 'algae-brown-burning')
 bobmods.lib.tech.add_recipe_unlock('bio-processing-brown', 'algae-brown-burning')
+
+-- Green algae
+bobmods.lib.tech.remove_recipe_unlock('bio-processing-green', 'algae-brown-burning')
 bobmods.lib.tech.remove_recipe_unlock('bio-processing-green', 'algae-brown-burning-wash')
-bobmods.lib.tech.add_recipe_unlock('bio-processing-brown', 'algae-brown-burning-wash')
 
 --[[
   Make Green Algae 2 available sooner:
@@ -17,14 +18,14 @@ bobmods.lib.tech.add_recipe_unlock('bio-processing-brown', 'algae-brown-burning-
   - Make Green Algae depend on Basic Chemistry instead of Water Treatment
 --]]
 bobmods.lib.tech.replace_prerequisite('bio-processing-red', 'bio-processing-green', 'bio-processing-blue')
-bobmods.lib.tech.remove_recipe_unlock('bio-processing-green', 'algae-farm-2')
-bobmods.lib.tech.add_recipe_unlock('bio-processing-blue', 'algae-farm-2')
+seablock.lib.moveeffect('algae-farm-2', 'bio-processing-red', 'bio-processing-blue', 1)
 bobmods.lib.tech.remove_prerequisite('bio-processing-green', 'bio-processing-brown')
 bobmods.lib.tech.replace_prerequisite('bio-processing-green', 'basic-chemistry', 'bio-wood-processing-2')
 
 -- Blue algae
 bobmods.lib.tech.replace_prerequisite('bio-processing-blue', 'bio-processing-red', 'bio-processing-brown')
 bobmods.lib.tech.remove_science_pack('bio-processing-blue', 'chemical-science-pack')
+bobmods.lib.tech.remove_recipe_unlock('bio-processing-blue', 'algae-farm-3')
 
 -- Make these craftable by hand
 data.raw.recipe['solid-alginic-acid'].category = "crafting"
@@ -43,3 +44,44 @@ data.raw.recipe['cellulose-fiber-algae'].energy_required = 2
 
 -- Speed up cellulose->wood pellet crafting
 data.raw.recipe['wood-pellets'].energy_required = 3
+
+-- Add prerequisite so Advanced algae processing isn't a dead end
+bobmods.lib.tech.add_prerequisite('sodium-processing', 'sb-advanced-algae-processing')
+
+-- Reduce cost of Algae farm 3
+
+local buildingmulti = angelsmods.marathon.buildingmulti
+local buildingtime = angelsmods.marathon.buildingtime
+
+angelsmods.functions.RB.build(
+  {
+    {
+      type = "recipe",
+      name = "algae-farm-3",
+      normal = {
+        energy_required = 5,
+        enabled = false,
+        ingredients = {
+          {type = "item", name = "algaefarm-3", amount = 1},
+          {type = "item", name = "t3-plate", amount = 11},
+          {type = "item", name = "t3-circuit", amount = 4},
+          {type = "item", name = "t3-brick", amount = 11},
+          {type = "item", name = "t3-pipe", amount = 18}
+        },
+        result = "algae-farm-3"
+      },
+      expensive = {
+        energy_required = 5 * buildingtime,
+        enabled = false,
+        ingredients = {
+          {type = "item", name = "algaefarm-3", amount = 1},
+          {type = "item", name = "t3-plate", amount = 11 * buildingmulti},
+          {type = "item", name = "t3-circuit", amount = 4 * buildingmulti},
+          {type = "item", name = "t3-brick", amount = 11 * buildingmulti},
+          {type = "item", name = "t3-pipe", amount = 18 * buildingmulti}
+        },
+        result = "algae-farm-3"
+      }
+    }
+  }
+)
