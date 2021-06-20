7720f79e0d46aa162ae12e7b3a231e8883159a9d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-06-20 14:42:06 +1200

    Remove setting override as it has been added in Angel's

--- SeaBlock/settings-updates/bobplates.lua
+++ SeaBlock/settings-updates/bobplates.lua
@@ -1,5 +1,4 @@
 -- Bob's Metals, Chemicals and Intermediates
 seablock.overwrite_setting('bool-setting', 'bobmods-plates-cheapersteel', false)
-seablock.overwrite_setting('bool-setting', 'bobmods-plates-newsteel', false)
 seablock.overwrite_setting('bool-setting', 'bobmods-plates-nuclearupdate', true)
 seablock.overwrite_setting('bool-setting', 'bobmods-plates-expensive-electrolysis', false)
