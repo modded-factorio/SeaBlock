8177f51779b69e81bac1dafdd6c1909160c9e0d2
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-07-16 23:34:35 +1200

    Settings changes #178

--- SeaBlock/settings-updates/bobassembly.lua
+++ SeaBlock/settings-updates/bobassembly.lua
@@ -3,7 +3,7 @@ if mods['bobassembly'] then
   seablock.overwrite_setting('bool-setting', 'bobmods-assembly-chemicalplants', false)
   seablock.overwrite_setting('bool-setting', 'bobmods-assembly-electrolysers', false)
   seablock.overwrite_setting('bool-setting', 'bobmods-assembly-distilleries', false)
+  seablock.overwrite_setting('bool-setting', 'bobmods-assembly-burner', false)
 
-  seablock.set_setting_default_value('bool-setting', 'bobmods-assembly-burner', false)
   seablock.set_setting_default_value('bool-setting', 'bobmods-assembly-oilfurnaces', false)
 end
--- SeaBlock/settings-updates/bobwarfare.lua
+++ SeaBlock/settings-updates/bobwarfare.lua
@@ -1,11 +1,6 @@
 -- Bob's Warfare
-if mods['bobenemies'] then
+if mods['bobwarfare'] then
   seablock.overwrite_setting('bool-setting', 'bobmods-warfare-robotupdate', false)
   seablock.overwrite_setting('bool-setting', 'bobmods-warfare-drainlesslaserturrets', false)
   seablock.overwrite_setting('bool-setting', 'bobmods-warfare-vehicleflamethrowerstartsfires', false)
-  seablock.overwrite_setting('bool-setting', 'bobmods-enemies-aliensdropartifacts', false)
-  seablock.overwrite_setting('double-setting', 'bobmods-enemies-leviathanfrequency', 0)
-  seablock.overwrite_setting('bool-setting', 'bobmods-enemies-biggersooner', false)
-  seablock.overwrite_setting('bool-setting', 'bobmods-enemies-superspawner', false)
-  seablock.overwrite_setting('bool-setting', 'bobmods-enemies-healthincrease', false)
 end
