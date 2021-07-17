59558c61b6f1a24d9c2ae15f3023735c11265996
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-07-17 23:03:15 +1200

    Removing old migration files
    Removing as they are causing errors for tips n tricks

--- SeaBlock/migrations/SeaBlock_0.2.0.lua
+++
@@ -1,7 +0,0 @@
-for _, force in pairs(game.forces) do
-  force.technologies['sb-startup1'].researched = true
-  force.technologies['sb-startup2'].researched = true
-  force.technologies['bio-wood-processing'].researched = true
-  --force.technologies['sb-startup3'].researched = true
-  force.technologies['sb-startup4'].researched = true
-end
