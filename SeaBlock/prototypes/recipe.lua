342a37f5c5f60e1be21549f6b0ecd5f5283a0e48
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-08-27 20:03:23 +1200

    Move icon creation from dff to data #43

--- SeaBlock/prototypes/recipe.lua
+++ SeaBlock/prototypes/recipe.lua
@@ -217,3 +217,49 @@ data:extend({
 
 bobmods.lib.tech.add_prerequisite('oil-gas-extraction', 'bio-processing-blue')
 bobmods.lib.tech.add_recipe_unlock('oil-gas-extraction', 'sb-blue-algae-liquefaction')
+
+-- Setup recipe bases
+
+local min_water_icons = angelsmods.functions.create_viscous_liquid_fluid_icon(nil, { {039,112,194}, {093,067,049}, {070,133,232}, {109,070,020,0.8} })
+
+-- Build Crystalizer slag processing recipes
+local composite_recipes = {}
+local slag_processing_list = {
+  ['sb-water-mineralized-crystallization'] = min_water_icons  
+}
+
+local slag_recipe_shifts = {
+  {-10, 10},
+  {10, 10}
+}
+
+for name,base_icons in pairs(slag_processing_list) do
+  -- Check the recipe exists
+  local recipe = data.raw.recipe[name]
+  if recipe then
+    local recipe_results
+    if recipe.normal then
+      recipe_results = recipe.normal.results
+    else
+      recipe_results = recipe.results
+    end
+
+    -- Build icon overlays based on recipe ingredients
+    if recipe_results[1].name ~= "angels-void" then
+      local shift_index = 1
+
+      -- Setup base layer
+      composite_recipes[name] = {["base"] = {icons = base_icons}}
+
+      -- Build icon overlays based on recipe products
+      for _, product in pairs(recipe_results) do
+        composite_recipes[name][product.name] = {shift = slag_recipe_shifts[shift_index], scale = 0.5}
+        shift_index = shift_index + 1
+      end
+    end
+  end
+end
+
+for name,sources in pairs(composite_recipes) do
+  seablock.reskins.composite_existing_icons(name, "recipe", sources)
+end
