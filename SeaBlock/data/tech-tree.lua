3cc4e0cb1c2a118017026341c8ed6ca343ad46a7
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-09-01 22:56:04 +1200

    Tech unlocks #206

--- SeaBlock/data/tech-tree.lua
+++ SeaBlock/data/tech-tree.lua
@@ -72,6 +72,7 @@ if mods['ScienceCostTweakerM'] then
   bobmods.lib.tech.add_prerequisite('sb-bio-processing-advanced', 'sct-bio-science-pack')
   bobmods.lib.tech.add_prerequisite('utility-science-pack', 'rubber')
 end
+bobmods.lib.tech.add_prerequisite('tungsten-alloy-processing', 'powder-metallurgy-3')
 
 -- Clear prerequisites so it's available at end of tutorial
 bobmods.lib.tech.remove_prerequisite('bio-wood-processing-2', 'bio-wood-processing')
