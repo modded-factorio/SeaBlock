e50c8fee6313ac6b8f8e4ae4ea8b4123c1b6e576
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-07-27 18:55:11 +1200

    SpaceX setting issue #273
    FTL Theory D was not ignoring tech cost modifier

--- SeaBlock/data-final-fixes/SpaceMod.lua
+++ SeaBlock/data-final-fixes/SpaceMod.lua
@@ -2,6 +2,46 @@ if not mods['SpaceMod'] then
   return
 end
 
+if settings.startup['SpaceX-ignore-tech-multiplier'] then
+  if settings.startup['SpaceX-ignore-tech-multiplier'].value then
+    for _, tech_name in pairs({
+      'ftl-theory-A',
+      'ftl-theory-B',
+      'ftl-theory-C',
+      'ftl-theory-D',
+      'ftl-theory-D1',
+      'ftl-theory-D2',
+      'ftl-propulsion'
+    }) do
+      local tech = data.raw.technology[tech_name]
+      
+      if tech then
+        tech.ignore_tech_cost_multiplier = true
+      end
+    end
+  else
+    for _, tech_name in pairs({
+      'space-assembly',
+      'space-construction',
+      'space-casings',
+      'protection-fields',
+      'fusion-reactor',
+      'space-thrusters',
+      'fuel-cells',
+      'habitation',
+      'life-support-systems',
+      'spaceship-command',
+      'astrometrics'
+    }) do
+      local tech = data.raw.technology[tech_name]
+      
+      if tech then
+        tech.unit.count = tech.unit.count / 4
+      end
+    end
+  end
+end
+
 local recipes = {
   'low-density-structure',
   'rocket-control-unit',
--- SeaBlock/data-updates/SpaceMod.lua
+++ SeaBlock/data-updates/SpaceMod.lua
@@ -1,44 +1,4 @@
 if mods['SpaceMod'] then
-  if settings.startup['SpaceX-ignore-tech-multiplier'] then
-    if settings.startup['SpaceX-ignore-tech-multiplier'].value then
-      for _, tech_name in pairs({
-        'ftl-theory-A',
-        'ftl-theory-B',
-        'ftl-theory-C',
-        'ftl-theory-D',
-        'ftl-theory-D1',
-        'ftl-theory-D2',
-        'ftl-propulsion'
-      }) do
-        local tech = data.raw.technology[tech_name]
-        
-        if tech then
-          tech.ignore_tech_cost_multiplier = true
-        end
-      end
-    else
-      for _, tech_name in pairs({
-        'space-assembly',
-        'space-construction',
-        'space-casings',
-        'protection-fields',
-        'fusion-reactor',
-        'space-thrusters',
-        'fuel-cells',
-        'habitation',
-        'life-support-systems',
-        'spaceship-command',
-        'astrometrics'
-      }) do
-        local tech = data.raw.technology[tech_name]
-        
-        if tech then
-          tech.unit.count = tech.unit.count / 4
-        end
-      end
-    end
-  end
-
   if settings.startup['bobmods-logistics-disableroboports'] and settings.startup['bobmods-logistics-disableroboports'].value then
     bobmods.lib.recipe.remove_ingredient('drydock-assembly', 'roboport')
     bobmods.lib.recipe.add_ingredients('drydock-assembly',
