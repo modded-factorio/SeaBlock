aecf70dfe2a20304dd28204381ee3f886ab7633f
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-11-27 13:57:36 +1300

    Reduced the cost of Circuit wires #286

--- SeaBlock/data-updates/rubber.lua
+++ SeaBlock/data-updates/rubber.lua
@@ -2,5 +2,5 @@ bobmods.lib.recipe.hide("bob-rubber")
 seablock.lib.moveeffect("insulated-cable", "electronics", "rubbers")
 
 -- Circuit network wires should not require rubber
-bobmods.lib.recipe.set_ingredients("green-wire", { { "electronic-circuit", 1 }, { "copper-cable", 1 } })
-bobmods.lib.recipe.set_ingredients("red-wire", { { "electronic-circuit", 1 }, { "copper-cable", 1 } })
+bobmods.lib.recipe.set_ingredients("green-wire", { { "solid-paper", 2 }, { "tinned-copper-cable", 1 } })
+bobmods.lib.recipe.set_ingredients("red-wire", { { "solid-paper", 2 }, { "tinned-copper-cable", 1 } })
