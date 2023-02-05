4339ca1f98eb0345703525624e02a869f4bf1d54
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-02-05 21:06:58 +1300

    Remove extra prerequisite

--- SeaBlock/data-final-fixes/tech-tree.lua
+++ SeaBlock/data-final-fixes/tech-tree.lua
@@ -18,6 +18,7 @@ bobmods.lib.tech.remove_recipe_unlock("angels-advanced-gas-processing", "solid-f
 bobmods.lib.tech.remove_prerequisite("circuit-network", "bio-wood-processing")
 bobmods.lib.tech.remove_recipe_unlock("circuit-network", "bob-rubber")
 bobmods.lib.tech.remove_recipe_unlock("circuit-network", "insulated-cable")
+bobmods.lib.tech.remove_prerequisite("rubbers", "circuit-network")
 
 -- Unhide solid fuel from hydrogen
 seablock.lib.unhide_recipe("solid-fuel-from-hydrogen")
