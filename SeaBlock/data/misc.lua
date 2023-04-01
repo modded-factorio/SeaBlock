d7ee4390c760e99d08e41e8cbe20287bd4d1d671
Adrian van der Wal <adrian@enigmatic.id.au>
2023-04-01 22:05:36 +0800

    Fixes #143

--- SeaBlock/data/misc.lua
+++ SeaBlock/data/misc.lua
@@ -1,27 +1,32 @@
-require("starting-items")
+require('starting-items')
 
 -- Populate table of starting items and script enabled techs for YAFC to read
 data.script_enabled = data.script_enabled or {}
 
 for k, _ in pairs(seablock.scripted_techs) do
-  table.insert(data.script_enabled, { type = "technology", name = k })
+  table.insert(data.script_enabled, {type = 'technology', name = k})
 end
 
 local starting_items = seablock.populate_starting_items(data.raw.item)
 for k, _ in pairs(starting_items) do
-  table.insert(data.script_enabled, { type = "item", name = k })
+  table.insert(data.script_enabled, {type = 'item', name = k})
 end
 
 -- Set Angel's triggers
-angelsmods.trigger.smelting_products["copper"].powder = true
-angelsmods.trigger.smelting_products["nickel"].plate = false
-angelsmods.trigger.smelting_products["zinc"].plate = false
-angelsmods.trigger.smelting_products["cobalt"].plate = false
-angelsmods.trigger.ores["platinum"] = true
-angelsmods.trigger.smelting_products["platinum"].plate = true
-angelsmods.trigger.smelting_products["platinum"].wire = true
-angelsmods.trigger.smelting_products["gunmetal"].plate = false
+angelsmods.trigger.smelting_products['copper'].powder = true
+angelsmods.trigger.smelting_products['nickel'].plate = false
+angelsmods.trigger.smelting_products['zinc'].plate = false
+angelsmods.trigger.smelting_products['cobalt'].plate = false
+angelsmods.trigger.ores['platinum'] = true
+angelsmods.trigger.smelting_products['platinum'].plate = true
+angelsmods.trigger.smelting_products['platinum'].wire = true
+angelsmods.trigger.smelting_products['gunmetal'].plate = false
 angelsmods.trigger.early_sulfuric_acid = true
 
 -- Copy Ore Processing Machine tech icon to Mechanical Refining
-seablock.lib.copy_icon(data.raw.technology["ore-crushing"], data.raw.technology["advanced-ore-refining-1"])
+seablock.lib.copy_icon(data.raw.technology['ore-crushing'], data.raw.technology['advanced-ore-refining-1'])
+
+-- Check if Bob's mining is not enabled, increase starting hand mining speed
+if not mods['bobmining'] then
+  data.raw.character.character.mining_speed = 0.75
+end
