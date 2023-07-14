7fc0ca43f2c246907d300983dfb558d11b898377
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-07-14 00:42:14 -0500

    Remove reference to deleted setting

--- SeaBlock/settings-updates/bobpower.lua
+++ SeaBlock/settings-updates/bobpower.lua
@@ -2,5 +2,4 @@
 if mods["bobpower"] then
   seablock.set_setting_default_value("bool-setting", "bobmods-power-accumulators", false)
   seablock.overwrite_setting("bool-setting", "bobmods-power-fluidgenerator", false)
-  seablock.overwrite_setting("bool-setting", "bobmods-power-burnergenerator", false)
 end
