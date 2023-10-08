8fbd44065e56a732c216062058c97ed23439f24d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-10-08 20:41:56 +1300

    Bio science pack now always requires green science

--- SeaBlock/settings-updates/ScienceCostTweakerM.lua
+++ SeaBlock/settings-updates/ScienceCostTweakerM.lua
@@ -2,5 +2,4 @@
 if mods["ScienceCostTweakerM"] then
   seablock.overwrite_setting("string-setting", "sct-lab-modules", "none")
   seablock.overwrite_setting("bool-setting", "sct-connect-science", false)
-  seablock.overwrite_setting("string-setting", "sct-bio", "tier1")
 end
