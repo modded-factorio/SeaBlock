7bcc3877faae9af7fab3979ae1d1667a98842c65
Daniel Meltzer <dmeltzer.devel@gmail.com>
2024-01-12 21:24:39 -0500

    Fix heavy pumps crashing new seablock maps (#317)
    * angelsrefining creates a custom resource for heavy water.  This code would remove that custom resource on new map gen, causing pumps to crash.
    
    * Fix heavy pumps crashing new seablock maps #317
    
    * Updating changelog #317
    
    ---------
    
    Co-authored-by: KiwiHawk <59639+KiwiHawk@users.noreply.github.com>

--- SeaBlock/data-updates/misc.lua
+++ SeaBlock/data-updates/misc.lua
@@ -65,10 +65,10 @@ end
 -- Remove resources so mining recipes don't show in FNEI
 -- Have to leave at least one resource or game will not load
 for k, v in pairs(data.raw["resource"]) do
-  if k == "coal" then
-    v.minable.result = nil
-    v.minable.results = nil
-  else
+  -- Sea-pump-resource is a virtual resource.
+  -- When the offshore pump is placed, it is supposed to be replaced by the resource and a mining-drill.
+  -- Removing the resource causes placement of heavy pumps to crash new maps.
+  if k ~= "sea-pump-resource" then 
     data.raw["resource"][k] = nil
   end
 end
